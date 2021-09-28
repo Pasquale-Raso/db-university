b568334347ace7ab154f6767a5da5788ec94dc41
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-28 15:19:44 +0200

    ex. n°3

--- University.sql
+++ University.sql
@@ -9,6 +9,9 @@ FROM `courses`
 WHERE `cfu` > "10"
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * 
+FROM `students`
+WHERE `date_of_birth` >= "1990-09-28"
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
