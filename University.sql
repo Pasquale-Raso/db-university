d743a7f230930d1277012d6fea277f18afb357cb
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-28 16:05:15 +0200

    ex. n° 7

--- University.sql
+++ University.sql
@@ -29,6 +29,8 @@ FROM `degrees`
 WHERE `level`= "magistrale"
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(*) AS "Dipartimenti Università"
+FROM `departments`
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
