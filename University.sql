a9c6195b0c763bd9547ddfc0ff22d53545199f92
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-28 15:48:56 +0200

    ex. n° 4

--- University.sql
+++ University.sql
@@ -14,6 +14,9 @@ FROM `students`
 WHERE `date_of_birth` >= "1990-09-28"
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT `period`, `year`
+FROM `courses`
+WHERE `period`= "I semestre" AND `year`= "1"
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
