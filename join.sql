d55ad82c9f07971e6ca11faed0f52cf1d105fde2
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-29 17:11:15 +0200

    join ex 6

--- join.sql
+++ join.sql
@@ -45,7 +45,17 @@ ORDER BY `students`.`surname`, `students`.`name`
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
-
+SELECT `teachers`.`surname`, `teachers`.`name`, `departments`.`name`
+FROM `departments`
+JOIN `degrees`
+ON   `departments`.`id` = `degrees`.`department_id`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'  
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 -- superare ciascuno dei suoi esami
