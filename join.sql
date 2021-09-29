5fab000034aedb3d42da2b8bfa61b8e3e9f85ad0
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-29 16:13:30 +0200

    join ex 1 - 2 - 3 - 4 - 5

---
+++ join.sql
@@ -0,0 +1,55 @@
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT * 
+FROM `students` 
+JOIN `degrees` 
+ON `students`.`degree_id` = `degrees`.`id` 
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT * 
+FROM `degrees` 
+JOIN `departments` 
+ON `degrees`.`department_id`= `departments`.`id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `teachers`.`name` AS "Nome ", `teachers`.`surname` AS "Cognome",`courses`.`name` AS "Nome Corso"
+FROM `courses` 
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `teachers` 
+ON `course_teacher`.`teacher_id` = `teachers`.`id` 
+WHERE `teachers`.`id` = 44
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT `students`.`surname`, `students`.`name`, `degrees`.`name`, `departments`.`name`
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `students`.`surname`, `students`.`name`, `degrees`.`name`, `departments`.`name`
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
+
+
+
+
